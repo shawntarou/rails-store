6e0d59a085df772531d3c137faab4d10ab4d9eee
shawn <173310929+shawntarou@users.noreply.github.com>
2026-08-30 10:57:47 -0700

    Define routes for Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+=begin
+  get "/products", to: "products#index" # show all records
+  get "/products/new", to: "products#new" # show form for creating new record
+  post "/products", to: "products#create" # process new form, handle errors, create record
+  get "/products/:id", to: "products#show" # show specific record
+  get "/products/:id/edit", to: "products#edit" # show form for updating record
+  put "/products/:id", to: "products#update" # handle edit form, handle errors, update ENTIRE record
+  patch "/products/:id", to: "products#update" # handle edit form, handle errors, update SPECIFIC attributes of record
+  delete "/products/:id", to: "products#destroy" # handle deleting specific record
+=end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :products # does everything above ^^^
 end
