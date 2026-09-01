0bf47145b3624e7620bacb6c786278178a7ed590
shawn <173310929+shawntarou@users.noreply.github.com>
2026-09-01 11:57:56 -0700

    Add price field to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_30_163737) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_01_184948) do
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
+    t.integer "price"
     t.datetime "updated_at", null: false
   end
 end
